e9e57223d261bbb85b8bff9f4fe0887283162f2f
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2023-06-16 15:23:34 +0200

    Fixing exports

--- src/index.ts
+++ src/index.ts
@@ -4,43 +4,131 @@ import { parseN3File } from './parser';
 const XSD  = 'http://www.w3.org/2001/XMLSchema#';
 const RDFS = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
 
-main();
+if (process.argv.length != 3) {
+    console.log(`usage: ${process.argv[1]} n3-file`);
+    process.exit(1);
+}
 
-async function main() : Promise<void> {
-    const store = await parseN3File('data/00-simple.n3');
-    const n3s = makeGraph(store, N3.DataFactory.defaultGraph());
+const input = process.argv[2];
+
+main(input);
+
+async function main(path: string) : Promise<void> {
+    const store = await parseN3File(path);
+    const graph = makeGraph(store, N3.DataFactory.defaultGraph());
+    const n3s = writeGraph(graph);
     console.log(n3s);
 }
 
 type IPSO = {
-    subject: ITerm | ITerm[] ,
-    predicate: ITerm | ITerm[] ,
-    object: ITerm | ITerm[],
+    type: 'PSO',
+    subject: ITerm ,
+    predicate: ITerm ,
+    object: ITerm ,
 };
 
-type ITerm = INamedNode | IBlankNode | ILiteral | IGraph;
+type ITerm = INamedNode | IBlankNode | ILiteral | IGraph | IList;
 
 type INamedNode = {
+    type: 'NamedNode';
     value: string;
+    datatype: null;
 };
 
 type IBlankNode = {
+    type: 'BlankNode';
     value: string;
+    datatype: null;
 };
 
 type ILiteral = {
+    type: 'Literal';
     value: string;
-    type: string;
+    datatype: string;
 };
 
-type IGraph = IPSO[] ;
+type IList = {
+    type: 'List';
+    value: ITerm[]
+    datatype: null;
+};
+
+type IGraph = {
+    type: 'Graph';
+    value: IPSO[];
+    datatype: null;
+};
 
 function pref(type: string, value: string) : string {
     return type + value;
 }
 
-function makeGraph(store: N3.Store, graph: N3.Term) : string {
-    const graphList : string[] = [];
+function writeGraph(graph: IGraph) : string {
+    const result : string[] = [];
+
+    graph.value.forEach( (pso) => {
+        const value = writePSO(pso);
+        result.push(value);
+    });
+
+    return result.join("\n");
+};
+
+function writePSO(pso: IPSO) : string {
+    const subject   = writeTerm(pso.subject);
+    const predicate = writeTerm(pso.predicate);
+    const object    = writeTerm(pso.object);
+
+    return `${predicate}(${subject},${object}).`;
+}
+
+function writeTerm(term: ITerm) : string {
+    if (term.type === 'NamedNode') {
+        return `'<${term.value}>'`;
+    }
+    else if (term.type === 'Literal') {
+        if (term.datatype === pref(XSD,'string')) {
+            return `'${term.value}'`;
+        }
+        else if (term.datatype === pref(XSD,'integer')) {
+            return term.value;
+        }
+        else if (term.datatype === pref(XSD,'boolean')) {
+            return term.value;
+        }
+        else {
+            return `literal('${term.value},'{${term.datatype}})`;
+        }
+    }
+    else if (term.type === 'BlankNode') {
+        return `'${term.value}'`;
+    }
+    else if (term.type === 'List') {
+        const result : string[] = [];
+        term.value.forEach( (li) => {
+            result.push(writeTerm(li));
+        });
+        return '[' + result.join(",") + ']';
+    }
+    else if (term.type === 'Graph') {
+        const result : string[] = [];
+        term.value.forEach( (gi) => {
+            const value = writePSO(gi);
+            result.push(value.replace(/\.$/,''));
+        });
+
+        return '(' + result.join(",") + ')';
+    }
+
+    return 'x';
+}
+
+function makeGraph(store: N3.Store, graph: N3.Term) : IGraph {
+    const result : IGraph = {
+        type: 'Graph',
+        value: [] as IPSO[],
+        datatype: null
+    };
 
     // First process the named nodes...
     store.forEach((quad) => {
@@ -49,7 +137,12 @@ function makeGraph(store: N3.Store, graph: N3.Term) : string {
             let subject   = parseTerm(quad.subject, store);
             let predicate = parseTerm(quad.predicate, store);
             let object    = parseTerm(quad.object, store);
-            graphList.push(`${predicate}(${subject},${object}).`);
+            result.value.push({
+                type: 'PSO',
+                subject: subject,
+                predicate: predicate,
+                object: object
+            } as IPSO);
         }
     }, null, null, null, graph);
 
@@ -61,53 +154,60 @@ function makeGraph(store: N3.Store, graph: N3.Term) : string {
             let subject   = parseTerm(quad.subject, store);
             let predicate = parseTerm(quad.predicate, store);
             let object    = parseTerm(quad.object, store);
-            graphList.push(`${predicate}(${subject},${object}).`);
+            result.value.push({
+                type: 'PSO', 
+                subject: subject,
+                predicate: predicate,
+                object: object
+            } as IPSO);
         }
     }, null, null, null, graph);
 
-    return graphList.join("\n");
+    return result;
 }
 
-function parseTerm(term: N3.Term, store: N3.Store) : string {
+function parseTerm(term: N3.Term, store: N3.Store) : ITerm {
     if (term.termType === 'NamedNode') {
         if (term.value === pref(RDFS,'nil')) {
-            return '[]';
+            return { type: 'List' , value: [] as ITerm[] } as IList;
         }
         else {
-            return `'<${term.value}>'`;
+            return { type: 'NamedNode' , value: term.value} as INamedNode;
         }
     }
     else if (term.termType === 'Literal') {
-        if (term.datatypeString === pref(XSD,'boolean')) {
-            return `${term.value}`;
-        }
-        else if (term.datatypeString === pref(XSD,'integer')) {
-            return `${term.value}`;
-        }
-        else {
-            return `literal('${term.value}',type('${term.datatypeString}')`;
-        }
+        return {
+            type: 'Literal',
+            value: term.value ,
+            datatype: term.datatypeString
+        } as ILiteral;
     }
     else if (term.termType === 'BlankNode') {
         if (isList(term,store)) {
             return makeList(term,store);
         }
         else if (isGraph(term,store)) {
-            return '(' + makeGraph(store,term) + ')';
+            return makeGraph(store,term);
         }
         else {
             const genid = makeGenId(term);
-            return `'<${genid}>'`;   
+            return {
+                type: 'BlankNode',
+                value: genid
+            } as IBlankNode;   
         }
     }
     else {
-        return '';
+        return {
+            type: 'BlankNode',
+            value: 'unknown'
+        } as IBlankNode;
     }
 }
 
 function makeGenId(term: N3.Term) : string {
-    const value = term.value.replace(/^.*(_|\.)/,'e_') ;
-    return `http://eyereasoner.github.io/.well-known/genid/2486415431719254739#${value}`;   
+    const value = term.value.replace(/^.*(_|\.)/,'') ;
+    return `_:${value}`;   
 }
 
 function isGraphLike(quad: N3.Quad, graph: N3.Term) : boolean {
@@ -152,8 +252,8 @@ function isGraph(term: N3.Term, store: N3.Store) : boolean {
     }
 }
 
-function makeList(term: N3.Term, store: N3.Store) : string {
-    let termList : string[] = [];
+function makeList(term: N3.Term, store: N3.Store) : IList {
+    let termList : ITerm[] = [];
     let searchTerm = term;
     let brake = false;
 
@@ -192,7 +292,5 @@ function makeList(term: N3.Term, store: N3.Store) : string {
         rest.forEach( (quad) => { store.removeQuad(quad) });
     } while (!brake);
     
-    return '[' + 
-        termList.join(",") +
-    ']';
+    return { type: 'List', value: termList } as IList;
 }
